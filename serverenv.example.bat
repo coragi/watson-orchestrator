303262d07c206c90405ab0be22ef0614859d9446
unknown <HaroldoOliveira@ATS.COM.BR>
2017-08-03 13:59:59 -0300

    Added root and port to the example

--- serverenv.example.bat
+++ serverenv.example.bat
@@ -7,4 +7,7 @@ set CONVERSATION_USER=your-conversation-username
 set CONVERSATION_PASS=your-conversation-password
 set CONVERSATION_WORKSPACE=your-conversation-workspace
 
+rem set STATIC_ROOT=client
+rem set PORT=3001
+
 node server.js
