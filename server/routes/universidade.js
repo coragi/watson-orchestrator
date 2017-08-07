dccd9adc492cce5c48a65debb952d611c1bce2de
unknown <HaroldoOliveira@ATS.COM.BR>
2017-08-07 15:38:31 -0300

    Redirecionando para a universidade.

--- server/routes/universidade.js
+++ server/routes/universidade.js
@@ -3,11 +3,20 @@
 const express = require('express');
 const router = express.Router();
 
+const request = require('request');
+
 router.get('/video/*', (req, res) => {
 	
 	let caminho = req.params[0];
 	console.info('Video no caminho ' + caminho);
 	
+	let caminhoCompleto = 'http://universidadeats.com.br/pluginfile.php/' + caminho;
+	request.get(caminhoCompleto, {
+		auth: {
+			user: process.env.UNIVERSIDADE_USER,
+			pass: process.env.UNIVERSIDADE_PASS
+		}
+	}).pipe(res);
 });
 
 module.exports = router;
