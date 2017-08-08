68258fc6a3d42e5b367a9f63a9c9a1d85d9a5af2
unknown <HaroldoOliveira@ATS.COM.BR>
2017-08-08 08:36:13 -0300

    Added parameter examples

--- serverenv.example.bat
+++ serverenv.example.bat
@@ -1,6 +1,13 @@
 @echo off
 
 set DISCOVERY_FLAG=true
+set DISCOVERY_USER=your-data-here
+set DISCOVERY_PASS=your-data-here
+set DISCOVERY_ENVIRONMENT=your-data-here
+set DISCOVERY_COLLECTION=your-data-here
+set DISCOVERY_OTHER_ENVIRONMENT=your-data-here
+set DISCOVERY_OTHER_CUPOM_COLLECTION=your-data-here
+
 
 set CONVERSATION_FLAG=true
 set CONVERSATION_USER=your-conversation-username
