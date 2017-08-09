d2d378ec7ca867d93aa9b8e33fb7e919c86c8427
unknown <HaroldoOliveira@ATS.COM.BR>
2017-08-09 09:44:52 -0300

    No default page.

--- server.js
+++ server.js
@@ -23,9 +23,11 @@ app.use('/api', api);
 app.use('/universidade', universidade);
 
 // Catch all other routes and return the index file
+/*
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+*/
 
 /**
  * Get port from environment and store in Express.
